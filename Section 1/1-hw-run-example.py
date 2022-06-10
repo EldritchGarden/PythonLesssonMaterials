b95443f0ffca2f1f17be560a26d95beeeee6d898
Eldritch-Hive <wolfrend013@gmail.com>
2022-06-10 09:33:07 -0400

    section 1

---
+++ Section 1/1-hw-run-example.py
@@ -0,0 +1,15 @@
+# This is your assignment this week.
+# These lines beginning with a # are comments.
+# Python ignores them, so you can write whatever you want.
+#
+# Your goal is to complete the missing part of the file in order
+# to get the proper output.
+# When you run the file you should see the message: "Peanut butter is illegal"
+#
+# If you get stuck, try to use the file 'run-me.py' that was shared in class
+# as reference
+
+message = "Peanut butter is illegal"
+
+# add the missing part below this line
+
---
+++ Section 1/run-me.py
@@ -0,0 +1,6 @@
+welcome = """
+Congrats! You successfully ran a python script!
+This message is proof that you managed to run a python program from the command line.
+"""
+
+print(welcome)
